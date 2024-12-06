924d021ddb8b311fedd1d7d50f419fce7f9aac12
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-06 20:31:45 +0100

    Create day_01.py

---
+++ day_01.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+print("Day 1")
+EXAMPLE = False
+
+file_name = f"2024/input/day_01"
+if EXAMPLE:
+    file_name += "_example"
+file_name += ".txt"
+
+with open(file_name, "r") as file:
+    numbers = tuple(zip(*(map(int, line.split()) for line in file)))
+
+solution = sum(
+    abs(n - m) for n, m in zip(sorted(numbers[0]), sorted(numbers[1]))
+)
+print(f"Part 1: {solution}")
+
+counts2 = Counter(numbers[1])
+solution = sum(n * counts2.get(n, 0) for n in numbers[0])
+print(f"Part 2: {solution}")
