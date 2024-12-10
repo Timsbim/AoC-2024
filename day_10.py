8ef321577e888fed298c1d9e40db3f366b91a8b5
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-10 07:49:20 +0100

    Update day_10.py

--- day_10.py
+++ day_10.py
@@ -6,32 +6,26 @@ if EXAMPLE:
     file_name += "_example"
 file_name += ".txt"
 with open(file_name, "r") as file:
-    topos = tuple(tuple(map(int, line.rstrip())) for line in file)
-
-ROWS, COLS = len(topos), len(topos[0])
-
-
-def moves(r, c):
-    for dr, dc in (0, 1), (1, 0), (0, -1), (-1, 0):
-        r1, c1 = r + dr, c + dc
-        if 0 <= r1 < ROWS and 0 <= c1 < COLS:
-            yield r1, c1
-
+    topos = {
+        (r, c): n
+        for r, line in enumerate(file)
+        for c, n in enumerate(map(int, line.rstrip()))
+    }
 
 score = count = 0
-for r0, row in enumerate(topos):
-    for c0, n in enumerate(row):
-        if n: continue
-        heads = [(r0, c0)]
-        for n in range(1, 10):
-            heads_new = []
-            for r, c in heads:
-                for r1, c1 in moves(r, c):
-                    if topos[r1][c1] == n:
-                        heads_new.append((r1, c1))
-            heads = heads_new
-        score += len(set(heads))
-        count += len(heads)
+for (r0, c0), n in topos.items():
+    if n: continue
+    heads = [(r0, c0)]
+    for n in range(1, 10):
+        heads_new = []
+        for r, c in heads:
+            for dr, dc in (0, 1), (1, 0), (0, -1), (-1, 0):
+                r1, c1 = r + dr, c + dc
+                if topos.get((r1, c1), -1) == n:
+                    heads_new.append((r1, c1))
+        heads = heads_new
+    score += len(set(heads))
+    count += len(heads)
 
 print(f"Part 1: {score}")
 print(f"Part 2: {count}")
