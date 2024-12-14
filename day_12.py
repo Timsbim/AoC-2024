4df195a5537b7793305fdecd31a2d98803424c82
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-14 16:32:21 +0100

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -1,6 +1,3 @@
-from operator import itemgetter
-
-
 print("Day 12")
 EXAMPLE = False
 
@@ -14,81 +11,34 @@ with open(file_name, "r") as file:
         for c, plant in enumerate(row.rstrip()):
             plants.setdefault(plant, set()).add((r, c))
 
-
-def plot_it(spots):
-    while spots:
-        p = spots.pop()
-        plot, rim = {p}, {p}
+DIRS = (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)
+count_1 = count_2 = 0
+for p, spots in plants.items():
+    rest = set(spots)
+    while rest:
+        s = rest.pop()
+        count = fences = sides = 0
+        rim = {s}
         while rim:
+            count += len(rim)
             rim_new = set()
             for r, c in rim:
-                for dr, dc in (0, 1), (1, 0), (0, -1), (-1, 0):
-                    p = r + dr, c + dc
-                    if p in spots:
-                        plot.add(p)
-                        spots.remove(p)
-                        rim_new.add(p)
+                ds, cs = [], []
+                for dr, dc in DIRS:
+                    ds.append((r + dr, c + dc))
+                    cs.append(ds[-1] not in spots)
+                for i in range(0, 7, 2):
+                    s = ds[i]
+                    if s in rest:
+                        rest.remove(s)
+                        rim_new.add(s)
+                    if s not in spots:
+                        fences += 1
+                        if cs[i-2] or not (cs[i-2] or cs[i-1]):
+                            sides += 1
             rim = rim_new
-        yield plot
-
-
-plots = tuple(plot for spots in plants.values() for plot in plot_it(spots))
-
-
-def holes(row):
-    return sum(b - a > 1 for a, b in zip(row, row[1:]))
-
-
-def fences(plot):
-    rows = {}
-    for r, c in plot:
-        rows.setdefault(r, []).append(c)
-    rows = tuple(sorted(rows[r]) for r in sorted(rows.keys()))
+        count_1 += count * fences
+        count_2 += count * sides
     
-    row_0 = rows[0]
-    if len(rows) == 1:
-        return 2 * (len(row_0) + holes(row_0) + 1)
-    
-    count = len(row_0) + 2 * (holes(row_0) + 1)
-    cols_0 = set(row_0)
-    for i, row_1 in enumerate(rows[1:], 1):
-        count += 2 * (holes(row_1) + 1)
-        cols_1 = set(row_1)
-        count += len(cols_0 ^ cols_1)
-        row_0, cols_0 = row_1, cols_1
-    
-    return count + len(row_1)
-    
-
-print(f"Part 1: {sum(len(plot) * fences(plot) for plot in plots)}")
-
-
-def sides(plot):
-    count = 0
-    for i, j in (0, 1), (1, 0):
-        rows, key_i, key_j = {}, itemgetter(i), itemgetter(j)
-        for p in plot:
-            rows.setdefault(key_i(p), []).append(key_j(p))
-        rows = tuple(rows[r] for r in sorted(rows.keys()))
-
-        row_0 = rows[0]
-        if i == 0 and (len(rows) == 1 or all(len(row) == 1 for row in rows)):
-            return 4
-        
-        count += holes(sorted(row_0)) + 1
-        cols_0 = set(row_0)
-        for i, row in enumerate(rows[1:], 1):
-            cols_1 = set(row)
-            c0, state = -2, True
-            for c1 in sorted(cols_0 ^ cols_1):
-                if c1 - c0 > 1 or (c1 in cols_1) != state:
-                    count += 1
-                    state = c1 in cols_1
-                c0 = c1
-            cols_0 = cols_1
-        count += holes(sorted(row)) + 1
-
-    return count
-
-
-print(f"Part 2: {sum(len(plot) * sides(plot) for plot in plots)}")
+print(f"Part 1: {count_2}")
+print(f"Part 2: {count_2}")
