ecbd7e88240c2eda9bd1e8fafb563066bffadea0
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-14 11:20:34 +0100

    Update day_14.py

--- day_14.py
+++ day_14.py
@@ -13,22 +13,15 @@ with open("2024/input/day_14.txt", "r") as file:
 robots = tuple(robots)
 ROWS, COLS = 103, 101
 
-
-def step(state, velocities):
-    return tuple(
-        ((py + vy) % ROWS, (px + vx) % COLS)
-        for (py, px), (vy, vx) in zip(state, velocities)
-    )
-
-
-state, velocities = zip(*robots)
-for _ in range(100):
-    state = step(state, velocities)
+state = tuple(
+    ((py + 100 * vy) % ROWS, (px + 100 * vx) % COLS)
+    for (py, px), (vy, vx) in robots
+)
 quadrants = {(0, 0): 0, (0, 1): 0, (1, 0): 0, (1, 1): 0}
 m_rows, m_cols = ROWS // 2, COLS // 2
 for y, x in state:
     if y != m_rows and x != m_cols:
-        quadrants[int(y < m_rows), int(x < m_cols)] += 1
+        quadrants[int(y < m_rows), int(x < m_cols)] += 1 
 print(f"Part 1: {prod(quadrants.values())}")
 
 
@@ -45,7 +38,10 @@ def contains_frame(state):
 
 state, velocities = zip(*robots)
 for s in range(1, 10404):
-    state = step(state, velocities)
+    state = tuple(
+        ((py + vy) % ROWS, (px + vx) % COLS)
+        for (py, px), (vy, vx) in zip(state, velocities)
+    )
     if contains_frame(state):
         solution = s
         break
