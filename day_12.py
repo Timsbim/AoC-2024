a93c145c97473a406fba87694a71e45de4a58c35
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-14 17:23:21 +0100

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -11,6 +11,7 @@ with open(file_name, "r") as file:
         for c, plant in enumerate(row.rstrip()):
             plants.setdefault(plant, set()).add((r, c))
 
+ds = (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)
 count_1 = count_2 = 0
 for p, spots in plants.items():
     rest = set(spots)
@@ -23,14 +24,14 @@ for p, spots in plants.items():
             rim_new = set()
             for r, c in rim:
                 for i in range(0, 7, 2):
-                    dr, dc = DIRS[i]
+                    dr, dc = ds[i]
                     s = r + dr, c + dc
                     if s in rest:
                         rest.remove(s)
                         rim_new.add(s)
                     if s not in spots:
                         fences += 1
-                        (dr2, dc2), (dr1, dc1) = DIRS[i-2], DIRS[i-1]
+                        (dr2, dc2), (dr1, dc1) = ds[i-2], ds[i-1]
                         b2 = (r + dr2, c + dc2) not in spots
                         b1 = (r + dr1, c + dc1) not in spots
                         if b2 or not (b2 or b1):
