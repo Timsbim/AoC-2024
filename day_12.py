4fe84a3e5c731b0f18f449dbef8974aba98b71dd
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-14 17:07:09 +0100

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -11,7 +11,6 @@ with open(file_name, "r") as file:
         for c, plant in enumerate(row.rstrip()):
             plants.setdefault(plant, set()).add((r, c))
 
-DIRS = (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)
 count_1 = count_2 = 0
 for p, spots in plants.items():
     rest = set(spots)
@@ -23,19 +22,19 @@ for p, spots in plants.items():
             count += len(rim)
             rim_new = set()
             for r, c in rim:
-                ds, cs = [], []
-                for dr, dc in DIRS:
-                    ds.append((r + dr, c + dc))
-                    cs.append(ds[-1] not in spots)
                 for i in range(0, 7, 2):
-                    s = ds[i]
+                    dr, dc = DIRS[i]
+                    s = r + dr, c + dc
                     if s in rest:
                         rest.remove(s)
                         rim_new.add(s)
                     if s not in spots:
                         fences += 1
-                        if cs[i-2] or not (cs[i-2] or cs[i-1]):
-                            sides += 1
+                        (dr2, dc2), (dr1, dc1) = DIRS[i-2], DIRS[i-1]
+                        b2 = (r + dr2, c + dc2) not in spots
+                        b1 = (r + dr1, c + dc1) not in spots
+                        if b2 or not (b2 or b1):
+                            sides += 1                             
             rim = rim_new
         count_1 += count * fences
         count_2 += count * sides
