2601b5edaa26e0b87b33ea9478ea0687eca0ee20
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-15 19:09:47 +0100

    Update day_02.py

--- day_02.py
+++ day_02.py
@@ -1,10 +1,7 @@
 print("Day 2")
 EXAMPLE = False
 
-file_name = f"2024/input/day_02"
-if EXAMPLE:
-    file_name += "_example"
-file_name += ".txt"
+file_name = f"day_02_input{'_example' if EXAMPLE else ''}.txt"
 with open(file_name, "r") as file:
     reports = tuple(tuple(map(int, line.split())) for line in file)
 
