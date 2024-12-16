b5ba5c547023db114f59ea4cbbfd1e4744fa3d13
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-16 08:58:43 +0100

    Update day_16.py

--- day_16.py
+++ day_16.py
@@ -36,13 +36,14 @@ while paths:
             if cost + 1 <= visited.get(pd, INF):
                 paths.append((path + (p1,), d, cost_new))
                 visited[pd] = cost_new
-    if (cost_new := cost + 1_000) < minimum:
+    if (cost_new := cost + 1_001) <= minimum:
         for d1 in (d - 1) % 4, (d + 1) % 4:
             dr, dc = DELTA[d1]
-            if GRID[r + dr, c + dc] != "#":
-                pd = path[-1], d1
+            if GRID[r1 := r + dr, c1 := c + dc] != "#":
+                p1 = r1, c1
+                pd = p1, d1
                 if cost_new <= visited.get(pd, INF):
-                    paths.append((path, d1, cost_new))
+                    paths.append((path + (p1,), d1, cost_new))
                     visited[pd] = cost_new
 solution_1, solution_2 = minimum, len(seats)
 
