ac7e413ee8d31bb5b6a41f52d395777db11f41fe
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-16 13:42:28 +0100

    Update day_16_v0.py

--- day_16_v0.py
+++ day_16_v0.py
@@ -7,28 +7,28 @@ EXAMPLE = False
 file_name = f"2024/input/day_16{'_example' if EXAMPLE else ''}.txt"
 with open(file_name, "r") as file:
     layout = file.read()
-GRID = set()
+grid = set()
 for r, row in enumerate(layout.splitlines()):
     for c, char in enumerate(row):
         if char == "S":
-            START, char = (r, c), "."
+            start, char = (r, c), "."
         elif char == "E":
-            END, char = (r, c), "."
+            end, char = (r, c), "."
         if char == ".":
-            GRID.add((r, c))
+            grid.add((r, c))
 
 DELTA = (0, 1), (1, 0), (0, -1), (-1, 0)
 INF = float("inf")
 
-minimum, paths, dists = INF, [(START, 0, 0)], {(START, 0): 0}
+minimum, paths, dists = INF, [(start, 0, 0)], {(start, 0): 0}
 while paths:
     paths_new = []
     for (r, c), d, cost in paths:
         for d, add in (d, 1), ((d - 1) % 4, 1_001), ((d + 1) % 4, 1_001):
             if (cost_new := cost + add) < minimum:
                 dr, dc = DELTA[d]
-                if (p := (r + dr, c + dc)) in GRID:
-                    if p == END:
+                if (p := (r + dr, c + dc)) in grid:
+                    if p == end:
                         minimum = cost_new
                     elif cost_new < dists.get(pd := (p, d), INF):
                         paths_new.append((p, d, cost_new))
@@ -36,17 +36,17 @@ while paths:
     paths = paths_new
 print(f"Part 1: {minimum}")
 
-seats, paths = set(), [((START,), 0, 0)]
+seats, paths = set(), [((start,), 0, 0)]
 while paths:
     path, d, cost = paths.pop()
     r, c = path[-1]
     for d, add in (d, 1), ((d - 1) % 4, 1_001), ((d + 1) % 4, 1_001):
         if (cost_new := cost + add) <= minimum:
             dr, dc = DELTA[d]
-            if (p := (r + dr, c + dc)) in GRID:
+            if (p := (r + dr, c + dc)) in grid:
                 path_new = path + (p,)
                 if cost_new == minimum:
-                    if p == END:
+                    if p == end:
                         seats.update(path_new)
                     continue
                 if cost_new == dists[p, d]:
