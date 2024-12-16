007560f68ded820dbcb96f7fee9d0c6d8de99b64
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-16 10:44:32 +0100

    Update day_16.py

--- day_16.py
+++ day_16.py
@@ -1,6 +1,7 @@
 print("Day 16")
 
 EXAMPLE = False
+
 file_name = f"2024/input/day_16{'_example' if EXAMPLE else ''}.txt"
 with open(file_name, "r") as file:
     layout = file.read()
@@ -16,28 +17,35 @@ for r, row in enumerate(layout.splitlines()):
 DELTA = (0, 1), (1, 0), (0, -1), (-1, 0)
 INF = float("inf")
 
-minimum, seats = INF, set()
-paths, visited = [((START,), 0, 0)], {(START, 0): 0}
+minimum, paths, visited = INF, [(START, 0, 0)], {(START, 0): 0}
+while paths:
+    paths_new = []
+    for (r, c), d, cost in paths:
+        for d, add in (d, 1), ((d - 1) % 4, 1_001), ((d + 1) % 4, 1_001):
+            if (cost_new := cost + add) < minimum:
+                dr, dc = DELTA[d]
+                if GRID[p := (r + dr, c + dc)] != "#":
+                    if p == END:
+                        minimum = cost_new
+                    elif cost_new < visited.get(pd := (p, d), INF):
+                        paths_new.append((p, d, cost_new))
+                        visited[pd] = cost_new
+    paths = paths_new
+print(f"Part 1: {minimum}")
+
+seats, paths = set(), [((START,), 0, 0)]
 while paths:
     path, d, cost = paths.pop()
     r, c = path[-1]
-    for d_new, addon in (d, 1), ((d - 1) % 4, 1_001), ((d + 1) % 4, 1_001):
-        if (cost_new := cost + addon) <= minimum:
-            dr, dc = DELTA[d_new]
+    for d, add in (d, 1), ((d - 1) % 4, 1_001), ((d + 1) % 4, 1_001):
+        if (cost_new := cost + add) <= minimum:
+            dr, dc = DELTA[d]
             if GRID[p := (r + dr, c + dc)] != "#":
                 path_new = path + (p,)
-                if p == END:
-                    if cost_new < minimum:
-                        minimum, seats = cost_new, set(path_new)
-                    else:
+                if cost_new == minimum:
+                    if p == END:
                         seats.update(path_new)
                     continue
-                if cost_new == minimum:
-                    continue
-                if cost_new <= visited.get(pd := (p, d_new), INF):
-                    paths.append((path_new, d_new, cost_new))
-                    visited[pd] = cost_new
-solution_1, solution_2 = minimum, len(seats)
-
-print(f"Part 1: {solution_1}")
-print(f"Part 2: {solution_2}")
+                if cost_new <= visited[p, d]:
+                    paths.append((path_new, d, cost_new))
+print(f"Part 2: {len(seats)}")
