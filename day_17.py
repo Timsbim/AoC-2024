c79029f49ca344c2346eb5eb4ddf84a3e87f785d
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-17 13:06:12 +0100

    Update day_17.py

--- day_17.py
+++ day_17.py
@@ -3,43 +3,42 @@ EXAMPLE = False
 
 file_name = f"2024/input/day_17{'_example' if EXAMPLE else ''}.txt"
 with open(file_name, "r") as file:
-    registers, program = file.read().split("\n\n")
-REGISTERS = {}
-for register in registers.splitlines():
+    blocks = file.read().split("\n\n")
+initial_state = {}
+for register in blocks[0].splitlines():
     _, reg, val = register.split()
-    REGISTERS[reg[0]] = int(val)
-PROGRAM = tuple(map(int, program.split()[1].split(",")))
+    initial_state[reg[0]] = int(val)
+PROGRAM = tuple(map(int, blocks[1].split()[1].split(",")))
 LENGTH = len(PROGRAM)
 
 REGS = {4: "A", 5: "B", 6: "C"}
 
 
-def step(registers, out, i):
+def step(state, out, i):
     op = PROGRAM[i + 1]
-    combo = op if op < 4 else registers[REGS[op]]
+    combo = op if op < 4 else state[REGS[op]]
     match PROGRAM[i]:
-        case 0: registers["A"] = registers["A"] // (2 ** combo)
-        case 1: registers["B"] = registers["B"] ^ op
-        case 2: registers["B"] = combo % 8
-        case 3: return i + 2 if registers["A"] == 0 else op
-        case 4: registers["B"] = registers["B"] ^ registers["C"]
+        case 0: state["A"] = state["A"] // (2 ** combo)
+        case 1: state["B"] = state["B"] ^ op
+        case 2: state["B"] = combo % 8
+        case 3: return i + 2 if state["A"] == 0 else op
+        case 4: state["B"] = state["B"] ^ state["C"]
         case 5: out.append(combo % 8)
-        case 6: registers["B"] = registers["A"] // (2 ** combo)  
-        case 7: registers["C"] = registers["A"] // (2 ** combo)             
+        case 6: state["B"] = state["A"] // (2 ** combo)  
+        case 7: state["C"] = state["A"] // (2 ** combo)             
 
     return i + 2
 
 
-registers = dict(REGISTERS)
-out, i = [], 0
-while 0 <= (i:= step(registers, out, i)) < LENGTH:
+state, out, i = initial_state, [], 0
+while 0 <= (i:= step(state, out, i)) < LENGTH:
     pass   
 print(f"Part 1: {','.join(map(str, out))}")
 
 
 def one_out(A):
-    out, registers, i = [], {"A": A, "B": 0, "C": 0}, 0
-    while 0 <= (i:= step(registers, out, i)) < LENGTH:
+    out, state, i = [], {"A": A, "B": 0, "C": 0}, 0
+    while 0 <= (i := step(state, out, i)) < LENGTH:
         if out:
             return out[0]
     
