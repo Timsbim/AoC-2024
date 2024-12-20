bcd4d90646431c1326f90ef351028451bbeac6dd
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-20 07:36:02 +0000

    Update day 20

--- run_all.py
+++ run_all.py
@@ -828,7 +828,6 @@ def day_19():
     with open(file_name, "r") as file:
         patterns, designs =  file.read().split("\n\n")
     PATTERNS = set(patterns.split(", "))
-    designs = tuple(designs.splitlines())
 
 
     @cache
@@ -840,9 +839,63 @@ def day_19():
         )
 
 
-    counts = [*map(accounting, designs)]
+    counts = [*map(accounting, designs.splitlines())]
     print(f"  - part 1: {sum(map(bool, counts))}")
     print(f"  - part 2: {sum(counts)}")
+
+
+def day_20():
+    print("Day 20:")
+
+    file_name = f"2024/input/day_20{'_example' if EXAMPLE else ''}.txt"
+    GRID = set()
+    with open(file_name, "r") as file:
+        for r, line in enumerate(file):
+            for c, char in enumerate(line):
+                if char == "S":
+                    START = r, c
+                elif char == "E":
+                    END = r, c
+                elif char == "#":
+                    GRID.add((r, c))
+    THRESHOLD = 50 if EXAMPLE else 100
+
+
+    def on_the_edge(p, dist):
+        r, c = p
+        yield r, c + dist
+        yield r, c - dist
+        for dr in range(1, dist):
+            dc = dist - dr
+            yield r + dr, c + dc
+            yield r - dr, c + dc
+            yield r + dr, c - dc
+            yield r - dr, c - dc
+        yield r + dist, c
+        yield r - dist, c
+
+
+    path = [None, START]
+    while path[-1] != END:
+        r, c = path[-1]
+        for dr, dc in (0, 1), (1, 0), (-1, 0), (0, -1):
+            p = r + dr, c + dc
+            if p != path[-2] and p not in GRID:
+                path.append(p)
+                break
+    path = {p: n for n, p in enumerate(path[1:])}
+    count_1 = count_2 = 0
+    for p0, t0 in path.items():
+        for dist in range(2, 21):
+            for p1 in on_the_edge(p0, dist):
+                if p1 in path:
+                    if path[p1] - t0 - dist >= THRESHOLD:
+                        if dist == 2:
+                            count_1 += 1
+                        count_2 += 1
+
+    print(f"  - part 1: {count_1}")
+    print(f"  - part 2: {count_2}")
 
 
 days = {
@@ -864,7 +917,8 @@ days = {
     16: day_16,
     17: day_17,
     18: day_18,
-    19: day_19
+    19: day_19,
+    20: day_20
 }
 
 
