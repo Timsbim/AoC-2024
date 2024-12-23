39128b507ed4036172be482d87a9c3195e85bd2b
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-23 12:44:06 +0000

    Update day 23

--- day_23.py
+++ day_23.py
@@ -5,26 +5,25 @@ file_name = f"2024/input/day_23{'_example' if EXAMPLE else ''}.txt"
 with open(file_name, "r") as file:
     EDGES = tuple(tuple(line.rstrip().split("-")) for line in file)
 
-nodes = {i: n for i, n in enumerate(sorted({n for e in EDGES for n in e}))}
-numbers, graph = {n: i for i, n in nodes.items()}, {n: [] for n in nodes}
-for n0, n1 in sorted((numbers[n0], numbers[n1]) for n0, n1 in EDGES):
+to_str = {i: n for i, n in enumerate(sorted({n for e in EDGES for n in e}))}
+to_int = {n: i for i, n in to_str.items()}
+graph = {n: set() for n in to_str} 
+for n0, n1 in sorted((to_int[n0], to_int[n1]) for n0, n1 in EDGES):
     if n0 < n1:
-        graph[n0].append(n1)
+        graph[n0].add(n1)
     else:
-        graph[n1].append(n0)
-num_nodes, neighbours = len(graph), {n: set(ns) for n, ns in graph.items()}
-cliques = [(n0, n1) for n0 in graph for n1 in graph[n0]]
-for length in range(3, num_nodes):
+        graph[n1].add(n0)
+bases = tuple(set(range(i + 1, len(graph))) for i in range(len(graph)))
+cliques = [(n0, n1) for n0, ns in graph.items() for n1 in ns]
+for length in range(3, len(graph)):
     cliques_new = []
     for clique in cliques:
-        for n1 in range(clique[-1] + 1, num_nodes):
-            if all(n1 in neighbours[n0] for n0 in clique):
-                cliques_new.append(clique + (n1,))
+        base = bases[clique[-1]].intersection(*(graph[n] for n in clique))
+        cliques_new.extend(clique + (n,) for n in base)
     if len(cliques_new) == 0: break
     cliques = cliques_new
     if length == 3:
-        solution_1 = sum(any(nodes[n][0] == "t" for n in c) for c in cliques)
-
+        solution_1 = sum(any(to_str[n][0] == "t" for n in c) for c in cliques)
 
 print("Part 1:", solution_1)
-print("Part 2:", ",".join(map(nodes.get, cliques[0])))
+print("Part 2:", ",".join(map(to_str.get, cliques[0])))
